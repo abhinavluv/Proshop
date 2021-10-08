c9d73b823fd968e7e886125039ca74692d6fd28a
abhinavluv19 <abhilav.sharma19@gmail.com>
2021-10-08 22:54:20 +0530

    Create header, footer and styling using react-bootstrap and page template using bootswatch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer.component';
+import Header from './components/Header.component';
+
 const App = () => {
     return (
         <>
-            <h1>Welcome to Proshop</h1>
+            <Header />
+            <main className='py-3'>
+                <Container>
+                    <h1>Welcome to Proshop</h1>
+                </Container>
+            </main>
+            <Footer />
         </>
     );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './bootstrap.min.css';
 import './index.css';
 import App from './App';
 
@@ -9,3 +10,6 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
+
+// bootstrap.min.css is one of the bootstrap templates fetched from https://bootswatch.com/
+// Above is Theme LUX
