cebda5a24718fdb98dc929378adb2b2a2ff47243
Abhinav <abhilav.sharma19@gmail.com>
2021-10-17 21:10:07 +0530

    create User, Product, Order schema and connection details

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,9 +5,11 @@ const connectDB = async () => {
     try {
         const connection = await mongoose.connect(process.env.MONGO_URI);
 
-        console.log(`MongoDB Connected: ${connection.connection.host}`);
+        console.log(
+            `MongoDB Connected: ${connection.connection.host}`.cyan.underline
+        );
     } catch (error) {
-        console.error(`Error: ${error.message}`);
+        console.error(`Error: ${error.message}`.red.underline.bold);
         process.exit(1);
     }
 };
