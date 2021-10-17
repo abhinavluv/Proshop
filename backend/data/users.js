2e61814f5416cc556bd466162876d8808754352d
Abhinav <abhilav.sharma19@gmail.com>
2021-10-17 21:20:21 +0530

    sample User data

---
+++ backend/data/users.js
@@ -0,0 +1,32 @@
+import bcrypt from ';bcryptjs';
+
+const users = [
+    {
+        name: 'Luv Sharma',
+        email: 'luv@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'Abhinav Sharma',
+        email: 'abhinav@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        name: 'Utsav Sharma',
+        email: 'utsav@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        name: 'Anita Sharma',
+        email: 'anita@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        name: 'Uday Kumar Sharma',
+        email: 'uday@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+];
+
+export default users;
