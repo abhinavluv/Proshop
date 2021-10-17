59a0854920fb67094174b743bfa34668bff43dd2
Abhinav <abhilav.sharma19@gmail.com>
2021-10-17 13:03:47 +0530

    Product List, product detail, navbar created and linked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Footer from './components/Footer.component';
 import Header from './components/Header.component';
+import HomeScreen from './screens/HomeScreen.component';
+import ProductScreen from './screens/ProductScreen.component';
 
 const App = () => {
     return (
-        <>
+        <BrowserRouter>
             <Header />
             <main className='py-3'>
                 <Container>
-                    <h1>Welcome to Proshop</h1>
+                    <Route path='/' component={HomeScreen} exact />
+                    <Route
+                        path='/product/:productId'
+                        component={ProductScreen}
+                        exact
+                    />
                 </Container>
             </main>
             <Footer />
-        </>
+        </BrowserRouter>
     );
 };
 
