46e27bb22c411666fa7c7285eb6a92cf0ecd996e
Abhinav <abhilav.sharma19@gmail.com>
2021-10-17 19:45:15 +0530

    Add database configuration

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    /*I figured this out. From the documentation, "useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options. Mongoose 6 always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false. Please remove these options from your code."*/
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(`MongoDB Connected: ${connection.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
